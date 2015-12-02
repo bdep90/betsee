1319895b4e5ab185624c5f52c61c418c39c6dd76
Bridgette Depay <BDep@Bridgette-Depays-MacBook-Pro.local>
2015-12-02 10:00:45 -0500

    sever now running on root route

--- server.js
+++ server.js
@@ -3,47 +3,57 @@ const express  = require('express');
 const mongoose = require('mongoose');
 const path     = require('path');
 const logger   = require('morgan');
-const bodyParser = require('body-parser');
+const parser   = require('body-parser');
 const app = express();
 
+// =======
+// parsing
+app.use(logger('dev'));
+app.use(parser.json()); // comes before routes
+app.use(parser.urlencoded({ extended: false}));
 
-// access routes
-const userRoutes = require('./routes/userRoutes');
+
+// ====
+// root
+app.get('/betsee', (req, res) => {
+  console.log('Hit Betsee');
+});
+
+// ================
+// access to routes
+// const userRoutes = require('./routes/userRoutes');
 // const projectRoutes = require('./routes/projectRoutes');
 // const patternRoutes = require('./routes/patternRoutes');
 // const etsyRoutes    = equire('./routes/etsyRoutes');
 
-app.use(logger('dev'));
-app.use(bodyParser.json()); // comes before routes
-
-app.use('/user', userRoutes); // register routes w/ express
+// app.use('/user', userRoutes); // register routes w/ express
 // app.use('/etsy', etsyRoutes); // ??
 
 
-// assigns paths
+// ====================
+// directory delegation
 app.use('/', express.static('public'));
 app.use('/scripts', express.static(__dirname + '/node_modules/angular'));
 app.use('/scripts', express.static(__dirname + '/node_modules/underscore'));
 
 
-// connect to db
-const db = mongoose.connection;
+// =============
+// db connection
 mongoose.connect('mongodb://localhost/betsee', (err) => {
   if (err) throw err;
 });
 
-
-// notification of open db
-const db = mongoose.connection;
+let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log('db connected');
+db.once('open', (callback) => {
+  console.log('mongoose connected');
 });
 
 
+// ======
 // server
 const server = app.listen(3000, () => {
   const host = server.address().address;
   const port = server.address().port;
-  console.log('Server now running.');
+  console.log('Server now running on ' + port);
 });
